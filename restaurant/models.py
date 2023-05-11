7932a966dee03cbd336c02b86fefd8c489f8d839
Artem <Artemvyhovanec@gmail.com>
2023-05-11 11:56:22 +0300

    create models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,61 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from restaurant.validator import validate_score
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Menu(models.Model):
+    date = models.DateField(auto_now_add=True)
+
+    restaurant = models.ForeignKey(
+        "Restaurant", on_delete=models.CASCADE, related_name="menu"
+    )
+    score = models.ManyToManyField(
+        "Employee", through="EmployeeScore", related_name="employee_score"
+    )
+
+
+class Employee(AbstractUser):
+    SERVER = "SR"
+    COOK = "CK"
+    DISHWASHER = "DH"
+    HOST_HOSTESS = "HH"
+    MANAGER = "MG"
+    BARTENDER = "BT"
+
+    POSITION_CHOICES = (
+        (SERVER, "Server"),
+        (COOK, "Cook"),
+        (DISHWASHER, "Dishwasher"),
+        (HOST_HOSTESS, "Host/Hostess"),
+        (MANAGER, "Manager"),
+        (BARTENDER, "Bartender"),
+    )
+
+    position = models.CharField(max_length=2, choices=POSITION_CHOICES)
+
+    score = models.ManyToManyField(
+        "Menu", through="EmployeeScore", related_name="menu_score"
+    )
+
+
+class EmployeeScore(models.Model):
+    employee = models.ForeignKey("Employee", on_delete=models.CASCADE)
+    menu = models.ForeignKey("Menu", on_delete=models.CASCADE)
+    points = models.IntegerField(validators=[validate_score])
+
+    class Meta:
+        unique_together = ["employee", "menu"]
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+
+    menu = models.ForeignKey(
+        "Menu", on_delete=models.CASCADE, related_name="menu_position"
+    )
